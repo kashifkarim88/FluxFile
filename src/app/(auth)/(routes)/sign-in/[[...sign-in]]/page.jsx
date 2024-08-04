5a3c7c010c9cc4ea362514fbf4814ca4029d0225
Kashif Karim <50681010+kashifkarim88@users.noreply.github.com>
2024-08-04 18:49:57 +0500

    Update page.jsx

--- src/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.jsx
+++ src/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.jsx
@@ -9,6 +9,7 @@ export default function SignInPage() {
                     <img
                         alt=""
                         src="https://images.unsplash.com/photo-1617195737496-bc30194e3a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                        width={500} height={300}
                         className="absolute inset-0 h-full w-full object-cover opacity-80"
                     />
                     <div className="hidden lg:relative lg:block lg:p-12">
