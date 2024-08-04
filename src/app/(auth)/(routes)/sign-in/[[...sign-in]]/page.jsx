7a4f574c845975e31aca7d73d992844b1f141d47
Kashif Karim <50681010+kashifkarim88@users.noreply.github.com>
2024-08-04 19:18:21 +0500

    Update page.jsx

--- src/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.jsx
+++ src/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.jsx
@@ -7,7 +7,7 @@ export default function SignInPage() {
         <section className="bg-white">
             <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
                 <section className="relative flex h-32 items-center bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-5">
-                    <img
+                    <Image
                         alt=""
                         src="https://images.unsplash.com/photo-1617195737496-bc30194e3a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
                         width={500} height={300}
@@ -15,7 +15,7 @@ export default function SignInPage() {
                     />
                     <div className="hidden lg:relative lg:block lg:p-12">
                         <h2 className="flex gap-3 mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-                            <img src={'/logo.svg'} width={30} height={30} alt="Logo" />
+                            <Image src={'/logo.svg'} width={30} height={30} alt="Logo" />
                             Welcome to FluxFile
                         </h2>
                         <p className="mt-4 leading-relaxed text-white/90">
